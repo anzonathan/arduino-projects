ff4c3ab7a4b41f2a04b6f7991e6090bb217e1e5a
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2024-12-19 16:59:27 +0300

    Create ladder_v1.ino

---
+++ ladder_v1.ino
@@ -0,0 +1,51 @@
+//Led Ladder Sequence 
+//Variation 1: With led going off before the next one
+
+//led number
+int l1 = 8;
+int l2 = 9;
+int l3 = 10;
+int l4 = 11;
+int l5 = 12;
+
+
+void setup() {
+
+  // Set all LED pins as outputs
+  pinMode(l1, OUTPUT);
+  pinMode(l2, OUTPUT);
+  pinMode(l3, OUTPUT);
+  pinMode(l4, OUTPUT);
+  pinMode(l5, OUTPUT);
+
+
+
+}
+
+void loop() {
+
+  digitalWrite(l1,HIGH);
+  delay(300);
+  digitalWrite(l1,LOW);
+  delay(100);
+  digitalWrite(l2,HIGH);
+  delay(300);
+  digitalWrite(l2,LOW);
+  delay(100);
+  digitalWrite(l3,HIGH);
+  delay(300);
+  digitalWrite(l3,LOW);
+  delay(100);
+  digitalWrite(l4,HIGH);
+  delay(300);
+  digitalWrite(l4,LOW);
+  delay(100);
+  digitalWrite(l5,HIGH);
+  delay(300);
+  digitalWrite(l5,LOW);
+  delay(100);
+
+
+
+
+}
