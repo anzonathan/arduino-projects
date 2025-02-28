381ef9c3c14dba70c51c8ecc958ea45cbe71b27d
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-02-28 13:46:23 +0300

    Create alarm.ino

---
+++ alarm.ino
@@ -0,0 +1,53 @@
+int LDR = A4;
+int Button = A5;
+
+int Gled = 13;
+int Rled = 12;
+int Buzzer = 11;
+
+int buttonState;
+int LDRState;
+
+void setup() {
+  Serial.begin(9600);
+
+  //Inputs
+  pinMode(LDR,INPUT);
+  pinMode(Button,INPUT);
+
+  //Outputs
+  pinMode(Buzzer,OUTPUT);
+  pinMode(Gled,OUTPUT);
+  pinMode(Rled,OUTPUT);
+  
+
+}
+
+void loop() {
+
+  //button input 
+  buttonState = digitalRead(Button);
+  //Serial.println(buttonState);
+  //delay(1000);
+
+  //LDR input
+  LDRState = analogRead(LDR);
+  Serial.println(LDRState);
+  delay(100);
+
+  if (LDRState < 20){
+    digitalWrite(Rled,HIGH);
+    digitalWrite(Gled,LOW);
+    tone(Buzzer,1000);
+
+
+  }
+
+  if ((LDRState > 20) || (buttonState == 1))  {
+    digitalWrite(Rled,LOW);
+    digitalWrite(Gled,HIGH);
+    noTone(Buzzer);
+    
+  }
+
+}
