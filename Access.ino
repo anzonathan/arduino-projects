60f1b680da883e0c1310e05e7b146f98b45910ed
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2025-03-18 14:27:03 +0300

    Add: UID String

--- Access.ino
+++ Access.ino
@@ -4,6 +4,7 @@
 #include <SPI.h> 
 #include <MFRC522.h>
 
+
 //lcd lib
 #include <LiquidCrystal_I2C.h>
 
@@ -17,6 +18,21 @@ LiquidCrystal_I2C lcd(0x27,  16, 2);
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
 MFRC522::MIFARE_Key key; 
+/*
+Jane
+A6 59 37 BB
+John
+08 33 AF 1B
+Jewel
+04 7C C6 DB
+Jasper
+C1 C0 CD CF
+*/
+
+//const int numOfCards = 4;
+
+//byte cards[numOfCards][4] = {{0xA6,0x59,0x37,0xBB},{0x08,0x33,0xAF,0x1B},{0x04,0x7C,0xC6,0xDB},{0xC1,0xC0,0xCD,0xCF}};
+//String names[numOfCards] = {"Jane","John","Jewel","Jasper"};
 
 void setup() { 
   Serial.begin(9600);
@@ -32,7 +48,7 @@ void setup() {
 }
  
 void loop() {
-
+  byte card_ID[4];//card UID size 4byte
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
@@ -43,24 +59,35 @@ void loop() {
 
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
 
-  Serial.print(F("RFID Tag UID:"));
-
-  printHex(rfid.uid.uidByte, rfid.uid.size);
+  //Serial.print(F("RFID Tag UID:"));
+  //printUID(rfid.uid.uidByte);
+  //Serial.println("UID :");
   
+  Serial.print(UID(rfid.uid.uidByte));
   Serial.println("");
 
+
+
+
   lcd.setCursor(0,2);
-  char UID[] = {rfid.uid.uidByte};
-  lcd.print(UID);
+  //char UID[] = {rfid.uid.uidByte};
+  //lcd.print(UID);
   
 
   rfid.PICC_HaltA(); // Halt PICC
 }
 
-//Routine to dump a byte array as hex values to Serial. 
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+//Funtion to return UID as a string
+String UID(byte *buffer) {
+  
+  String index = "";
+
+  for (byte i = 0; i < 4; i++) {
+
+    String bit = String(buffer[i], HEX);
+    index.concat(bit);
   }
+
+ return index;
 }
+
